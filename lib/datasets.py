cea054de4149495ad875e28725a2e4675813d111
Ruslan Grimov <grimovr@gmail.com>
2020-05-10 18:28:12 +0300

    Datasets with clean patches

--- lib/datasets.py
+++ lib/datasets.py
@@ -1,3 +1,5 @@
+import pickle
+
 import numpy as np
 import pandas as pd
 
@@ -9,6 +11,13 @@ wsi_csv_path = "/mnt/HDDData/pdata/train.csv"
 
 patches_path = "/mnt/SSDData/pdata/processed/patches_512/"
 patches_csv_path = "/mnt/SSDData/pdata/processed/patches_512.csv"
+patches_clean90_csv_path = "/mnt/SSDData/pdata/processed/patches_clean90_512.csv"
+
+patches_pkl_path = "/mnt/SSDData/pdata/processed/patches_512.pkl"
+patches_clean90_pkl_path = "/mnt/SSDData/pdata/processed/patches_clean90_512.pkl"
+
+# Here is the list of tiff files which are probably corrupt
+wrong_img_ids_path = '/mnt/SSDData/pdata/processed/wrong_img_ids.pkl'
 
 patch_sz = 512
 
@@ -43,6 +52,9 @@ patches_rgb_std_av1 = np.array([0.15, 0.26, 0.18])[None, :, None, None]
 
 
 def get_train_test_img_ids_split(test_size=0.2, random_state=42):
+    with open(wrong_img_ids_path, 'rb') as f:
+        wrong_img_ids = pickle.load(f)
+
     df = pd.read_csv(wsi_csv_path)
     gr_by_id_df = df.groupby('image_id').count()['data_provider']
     img_ids = gr_by_id_df.index.values.tolist()
@@ -51,6 +63,9 @@ def get_train_test_img_ids_split(test_size=0.2, random_state=42):
     train_img_ids, test_img_ids = train_test_split(img_ids,
                                                    test_size=test_size,
                                                    random_state=random_state)
+
+    train_img_ids = [a for a in train_img_ids if a not in wrong_img_ids]
+    test_img_ids = [a for a in test_img_ids if a not in wrong_img_ids]
 
     return train_img_ids, test_img_ids
 
