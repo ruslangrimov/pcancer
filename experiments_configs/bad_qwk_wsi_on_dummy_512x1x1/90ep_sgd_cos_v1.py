a5fa3603e44992e0bde0f2d77aaaa28d008ca2c3
Ruslan Grimov <grimovr@gmail.com>
2020-05-14 23:22:37 +0300

    Experiments with qwk wrongly calculated

---
+++ experiments_configs/bad_qwk_wsi_on_dummy_512x1x1/90ep_sgd_cos_v1.py
@@ -0,0 +1,35 @@
+import os, sys
+sys.path.append(os.path.dirname(__file__))
+import _default as d
+
+d.epochs = 90
+d.warmup_steps = 0
+
+hparams = {
+    'epochs': d.epochs,
+    'learning_rate': 0.04 * d.batch_size / 256,
+    'optimizer': {
+        'name': 'torch.optim.SGD',
+        'params': {
+            'weight_decay': 1e-4
+        }
+    },
+    'scheduler': {
+        'name': 'torch.optim.lr_scheduler.CosineAnnealingLR',
+        'params': {
+            'T_max': d.epochs * d.steps_in_epoh - d.warmup_steps,
+        },
+        'interval': 'step'
+    },
+    'source_code': open(__file__, 'rt').read()
+}
+
+d.hparams.update(hparams)
+
+
+def get_hrapams():
+    return d.get_hrapams()
+
+
+def update_hrapams(hparams, steps_in_epoh):
+    return d.update_hrapams(hparams, steps_in_epoh)
---
+++ experiments_configs/bad_qwk_wsi_on_dummy_512x1x1/90ep_sgd_exp_v3.py
@@ -0,0 +1,36 @@
+import os, sys
+sys.path.append(os.path.dirname(__file__))
+import _default as d
+
+d.epochs = 90
+d.warmup_steps = 0
+
+hparams = {
+    'epochs': d.epochs,
+    'learning_rate': 0.04 * d.batch_size / 256,
+    'optimizer': {
+        'name': 'torch.optim.SGD',
+        'params': {
+            'momentum': 0.9,
+            'weight_decay': 1e-4
+        }
+    },
+    'scheduler': {
+        'name': 'torch.optim.lr_scheduler.ExponentialLR',
+        'params': {
+            'gamma': 0.92,
+        },
+        'interval': 'epoch'
+    },
+    'source_code': open(__file__, 'rt').read()
+}
+
+d.hparams.update(hparams)
+
+
+def get_hrapams():
+    return d.get_hrapams()
+
+
+def update_hrapams(hparams, steps_in_epoh):
+    return d.update_hrapams(hparams, steps_in_epoh)
