83e8c6754cc0ce62e45628adf48cec57aa00e236
Ruslan Grimov <grimovr@gmail.com>
2020-05-17 14:20:31 +0300

    H&E augmentations

--- lib/augmentations.py
+++ lib/augmentations.py
@@ -1,4 +1,6 @@
+import numpy as np
 import albumentations as alb
+import staintools
 
 _aug_v1 = alb.Compose([
     alb.RandomRotate90(),
@@ -25,6 +27,39 @@ _aug_v1 = alb.Compose([
     ], p=0.5),
 ], p=0.9)
 
+_aug_v1_wo_blur = alb.Compose([
+    alb.RandomRotate90(),
+    alb.Flip(),
+    alb.Transpose(),
+    alb.ShiftScaleRotate(shift_limit=0.0625,
+                         scale_limit=0.2,
+                         rotate_limit=0,
+                         p=0.5),
+    # alb.CLAHE(clip_limit=2),
+    alb.RandomContrast(p=0.3),
+    alb.RandomBrightness(p=0.3),
+    alb.RandomGamma(p=0.3),
+    alb.HueSaturationValue(p=0.3),
+    alb.RGBShift(p=0.3)
+], p=0.9)
+
+_aug_v1_wo_blur_and_clr = alb.Compose([
+    alb.RandomRotate90(),
+    alb.Flip(),
+    alb.Transpose(),
+    alb.ShiftScaleRotate(shift_limit=0.0625,
+                         scale_limit=0.2,
+                         rotate_limit=0,
+                         p=0.5)
+], p=0.9)
+
+_aug_v1_only_scale = alb.Compose([
+    alb.ShiftScaleRotate(shift_limit=0.0,
+                         scale_limit=0.2,
+                         rotate_limit=0,
+                         p=0.5)
+], p=0.9)
+
 _aug_v1_clr_only = alb.Compose([
     alb.ShiftScaleRotate(shift_limit=0.0,  # ToDo: Not sure
                          scale_limit=0.2,
@@ -45,6 +80,31 @@ _aug_v1_clr_only = alb.Compose([
         alb.Blur(blur_limit=11, p=0.2),
     ], p=0.5),
 ], p=0.9, additional_targets={f'image{i}': 'image' for i in range(1000)})
+
+
+normalizer = staintools.StainNormalizer(method='macenko')
+
+
+def normalize_he(img, mask):
+    img = np.clip(normalizer.transform(img), 0, 255)
+
+    img = img / 255
+
+    return img, mask
+
+
+def augment_he(imgs, mask):
+    raise NotImplementedError("Not implemented for wsi yet")
+
+
+def augment_he_D8(img, mask):
+    augmentor = staintools.StainAugmentor(method='macenko', sigma1=0.4,
+                                          sigma2=0.4, augment_background=False)
+    augmentor.fit(img)
+
+    img = np.clip(augmentor.pop(), 0, 255).astype(np.uint8)
+
+    return _augment(img, mask, _aug_v1_wo_blur_and_clr)
 
 
 def augment_empty(img, mask):
@@ -84,6 +144,10 @@ def _augment_clr_only(imgs, aug):
 
 def augment_v1(img, mask):
     return _augment(img, mask, _aug_v1)
+
+
+def augment_v1_wo_blur(img, mask):
+    return _augment(img, mask, _aug_v1_wo_blur)
 
 
 def augment_v1_clr_only(imgs):
